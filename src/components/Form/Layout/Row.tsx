b1c8face631c0c369ebfc97e7b5b330acfe99512
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2022-11-29 17:11:56 +0400

    Add form layout components

---
+++ src/components/Form/Layout/Row.tsx
@@ -0,0 +1,20 @@
+import { Grid, GridProps } from "@mui/material";
+import { DEFAULT_FORM_COLUMN_SPACING } from "constants/layuot";
+
+
+const FormRow: React.FC<Partial<GridProps>> = ({ children, ...gridProps }) => {
+  return (
+    <Grid
+      columnSpacing={DEFAULT_FORM_COLUMN_SPACING}
+      alignItems="center"
+      {...gridProps}
+      item
+      container
+      direction="row"
+    >
+      {children}
+    </Grid>
+  );
+};
+
+export default FormRow;
