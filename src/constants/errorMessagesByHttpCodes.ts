16c6b62c1e5f5f85f863caa46a5043d40130f703
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2022-12-02 15:26:25 +0400

    Update constants

---
+++ src/constants/errorMessagesByHttpCodes.ts
@@ -0,0 +1,8 @@
+export const ERRORS_BY_HTTP_CODE = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+  413: "Payload Too Large",
+  500: "Internal Server Error",
+};
