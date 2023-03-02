e11aa8c432c55e3f26b4adc8fc3591f4d08bfc3c
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2023-03-02 15:56:34 +0400

    [Fix] save constants (#7)

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,3 +1,4 @@
+export const DATE_PARSE_FORMATS = ['YYYY-MM-DD', 'YYYY-MM-DD HH:mm:ss']
 export const DEFAULT_DATE_FORMAT = "DD.MM.YYYY";
 export const DEFAULT_TIME_FORMAT = "HH:mm";
 export const DEFAULT_DATETIME_FORMAT = "DD.MM.YYYY HH:mm";
