47a6649477e2677bfd224c2511009c003e54b88c
Pavel Tyurikov <pavel.tyurikov@gmail.com>
2024-04-13 12:46:31 +0300

    Remove directories

--- src/components/Layout/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@mui/material";
-import { ReactNode } from "~/lib/types";
-
-
-const Header = ({ children }: ReactNode) => {
-  return <Box component="header">{children}</Box>;
-};
-
-export default Header;
